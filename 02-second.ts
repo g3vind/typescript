6c21e17fe7575547ab4ef14fac35fdc2a122b03e
g3vind <g3vind@gmail.com>
2023-12-21 23:04:17 +0530

    first commit

---
+++ 02-second.ts
@@ -0,0 +1,13 @@
+// 1: wap to add two numbers
+
+// function definition
+function sum(num1:number = 4, num2:number = 5){
+   console.log(num1+num2)
+}
+
+// function call
+sum() // 9
+sum(2,3)  // 5
+
+// 2: how to catch errors and solve it
+// 3: ts configuration file
